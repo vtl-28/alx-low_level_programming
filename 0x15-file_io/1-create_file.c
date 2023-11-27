39e1577935c8b431dfab9686786d450dc624cec4
Vuyisile <vtlehola23@gmail.com>
2023-11-27 05:44:18 -0800

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,12 +21,13 @@ int create_file(const char *filename, char *text_content)
 	if (empty == NULL)
 		return (-1);
 
-	file = open(filename,  O_WRONLY | O_CREAT, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (!file)
 		return (-1);
 
 	write(file, text_content, strlen(text_content));
+	close(file);
 
 	return (1);
 
